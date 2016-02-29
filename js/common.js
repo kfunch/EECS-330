718745d31e1d04e9b2ce5858343f20a812e74756
crazydonkey200 <crazydonkey200@gmail.com>
2016-02-29 15:20:14 -0600

    Added login popup to login button in all pages.

---
+++ js/common.js
@@ -0,0 +1,20 @@
+document.body.innerHTML += '<div class="modal fade" id="login-modal" tabindex="-1" role="dialog" \
+         aria-labelledby="myModalLabel" aria-hidden="true" style="display: none;"> \
+      <div class="modal-dialog"> \
+	<div class="loginmodal-container"> \
+	  <h1>Welcome to NU :)</h1><br> \
+	  <form> \
+	    <input type="text" name="user" placeholder="Username"> \
+	    <input type="password" name="pass" placeholder="Password"> \
+	    <input type="submit" name="login" class="login loginmodal-submit" value="Login"> \
+	  </form> \
+	  <div class="login-help"> \
+	    <a href="#">Register</a> - <a href="#">Forgot Password</a> \
+	  </div> \
+	</div> \
+      </div> \
+</div>'
+
+login = document.getElementById('login');
+login.setAttribute('data-toggle', "modal");
+login.setAttribute('data-target', "#login-modal");
