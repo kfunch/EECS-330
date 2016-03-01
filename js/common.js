c3d963dbd51e6ccea6c745eb7600fcb286b074f2
crazydonkey200 <crazydonkey200@gmail.com>
2016-02-29 23:23:42 -0600

    Bug fix: Moved the html code for login window to the beginning of the js.

--- js/common.js
+++ js/common.js
@@ -1,16 +1,4 @@
-login = document.getElementById('login');
-register = document.getElementById('register');
-
-if (document.cookie && (document.cookie != "not set")) {
-    register.innerHTML = '<span class="glyphicon glyphicon-user"></span> ' + document.cookie;
-    login.innerHTML = '<span class="glyphicon glyphicon-log-in"></span> Logout';
-    login.onclick = function () {
-        document.cookie = "not set";
-        location.reload();
-    }
-}
-else {
-    document.body.innerHTML += '<div class="modal fade" id="login-modal" tabindex="-1" role="dialog" \
+document.body.innerHTML += '<div class="modal fade" id="login-modal" tabindex="-1" role="dialog" \
          aria-labelledby="myModalLabel" aria-hidden="true" style="display: none;"> \
       <div class="modal-dialog"> \
 	<div class="loginmodal-container"> \
@@ -25,7 +13,20 @@ else {
 	  </div> \
 	</div> \
       </div> \
-</div>'    
+</div>'
+
+login = document.getElementById('login');
+register = document.getElementById('register');
+
+if (document.cookie && (document.cookie != "not set")) {
+    register.innerHTML = '<span class="glyphicon glyphicon-user"></span> ' + document.cookie;
+    login.innerHTML = '<span class="glyphicon glyphicon-log-in"></span> Logout';
+    login.onclick = function () {
+        document.cookie = "not set";
+        location.reload();
+    }
+}
+else {
     login.setAttribute('data-toggle', "modal");
     login.setAttribute('data-target', "#login-modal");
     submit_button = document.getElementsByName('login')[0];
